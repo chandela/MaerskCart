91e3370c2e1dffdd494b63f01e24e263f137448f
Hari Kumar <harireddydear@gmail.com>
2020-06-09 23:43:58 -0700

    Starting Commit1

---
+++ MaerskCartApp/CartModels/CartItemsModel.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace MaerskCartApp.CartModels
+{
+    public class CartItemsModel
+    {
+        /// <summary>
+        /// ProductName
+        /// </summary>
+        public string ItemName { get; set; }
+
+        /// <summary>
+        /// Product Sku
+        /// </summary>
+        public int ItemSkuNumbers { get; set; }
+
+        /// <summary>
+        /// Product Cost
+        /// </summary>
+        public int ItemCost { get; set; }
+    }
+}
---
+++ MaerskCartApp/Controllers/CartController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaerskCartApp.Business;
+using MaerskCartApp.BusinessEntities;
+using MaerskCartApp.CartModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MaerskCartApp.Controllers
+{
+    //[Route("[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+
+        private readonly ICartProcessor cartProcessor;
+
+        private List<CartItems> Cartlist;
+        public CartController(ICartProcessor cartProcessor)
+        {
+            this.cartProcessor = cartProcessor;
+        }
+        [HttpGet]
+        [Route("GetCartList")]
+        public List<CartItems> GetCartList()
+        {
+           if(Cartlist == null)
+            {
+                GetFakeData();
+                //Cartlist = this.Cartlist;
+            }
+
+            //Explicit class Invocation instead Dependency Resolver
+            cartProcessor.ProcessCartdata(Cartlist);
+
+
+            return Cartlist;
+        }
+
+        [HttpPost]
+        [Route("GetCartList")]
+        public List<CartItems> GetCartList([FromBody] List<CartItems> cartitems)
+        {
+            if (Cartlist == null)
+            {
+                GetFakeData();
+            }
+
+            cartProcessor.ProcessCartdata(Cartlist);
+
+            return Cartlist;
+        }
+
+
+        [Route("GetData")]
+        public void GetFakeData()
+        {
+            Cartlist = new List<CartItems>()
+                {
+                    new CartItems
+                    {
+                        ItemName = "A",
+                        ItemCost = 50,
+                        ItemSkuNumbers=4
+                    },
+
+                    new CartItems
+                    {
+                        ItemName = "B",
+                        ItemCost = 30,
+                        ItemSkuNumbers=3
+                    },
+
+                    new CartItems
+                    {
+                        ItemName = "C",
+                        ItemCost = 20,
+                        ItemSkuNumbers=2
+                    },
+                    new CartItems
+                    {
+                        ItemName = "D",
+                        ItemCost = 15,
+                        ItemSkuNumbers=1
+                    }
+                };
+        }
+    }
+}
